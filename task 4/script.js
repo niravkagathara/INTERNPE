bc97c956d0faa633e78dfb870c9f2b9806a66ec6
Nirav kagathara <121386601+niravkagathara@users.noreply.github.com>
2024-07-11 16:33:56 +0530

    Add files via upload

---
+++ task 4/script.js
@@ -0,0 +1,119 @@
+var gameOver = false;
+var board;
+var pred = "R";
+var pyellow = "Y";
+var current = pred;
+
+var rows = 6;
+var columns = 7;
+var currColumns = [];
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(' ');
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
+    }
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    r = currColumns[c]; 
+
+    if (r < 0) { 
+        return;
+    }
+
+    board[r][c] = current; 
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (current == pred) {
+        tile.classList.add("red-piece");
+        current = pyellow;
+    }
+    else {
+        tile.classList.add("yellow-piece");
+        current = pred;
+    }
+
+    r -= 1; 
+    currColumns[c] = r; 
+
+    checkWinner();
+}
+
+function checkWinner() {
+     for (let r = 0; r < rows; r++) {
+         for (let c = 0; c < columns - 3; c++){
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+         }
+    }
+
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    if (board[r][c] == pred) {
+        winner.innerText = "pink Wins";             
+    } else {
+        winner.innerText = "orange Wins";
+    }
+    gameOver = true;
+}
